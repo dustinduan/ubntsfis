f2ea0fdc72d2b0a36b0554f0ee8878eaf45e2d7d
dustinduan2010 <dustinduan2010@163.com>
2018-02-28 10:55:43 +0800

    delete gbk code

--- string-search.py
+++ string-search.py
@@ -1,9 +1,9 @@
 import os
 def str_search():
 	filelist=os.listdir()
-	str_tar=input("请输入需要搜索的特征字符:")
+	str_tar=input("Please input the searching string:")
 	for x in filelist:
-		if "search" not in x:
+		if "search" and ".git" not in x:
 			with open(x,"r") as f:
 				for each_line in f:
 					if str_tar in each_line:
