349b4754d1bcabe227c158b21bcd73674c80b264
dustinduan2010 <dustinduan2010@163.com>
2018-02-28 10:12:56 +0800

    final str search

--- string-search.py
+++ string-search.py
@@ -1,13 +1,12 @@
 import os
-import platform as pl
 def str_search():
-    str_tar=input("请输入需要搜索的特征信息:")
-    os.system("cls")
-    for x in os.listdir():
-        with open(x,"r") as f:
-            i=0
-            for each_line in f and i==0:
-                if str_tar in each_line:
-                    print(x)
-                    i=1
+	filelist=os.listdir()
+	str_tar=input("请输入需要搜索的特征字符:")
+	for x in filelist:
+		if "search" not in x:
+			with open(x,"r") as f:
+				for each_line in f:
+					if str_tar in each_line:
+						print(x)
+						break
 str_search()
