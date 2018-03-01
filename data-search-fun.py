b668a7173865c893999e60f98e4a98330429391b
dustinduan2010 <dustinduan2010@163.com>
2018-03-01 09:54:29 +0800

    add data delay fun

--- data-search-fun.py
+++ data-search-fun.py
@@ -1,8 +1,20 @@
 import os
+import time
+import platform as pl
+def clean_screen():
+	if 'Windows' in pl.platform():
+		os.system("cls")
+	else:
+		os.system("clear screen")
+def delay():
+	time.sleep(3)
+	clean_screen()
 def data_search(file="operation.txt"):	
 	lead_tar=input("Please input the searching product SN or MAC:")
 	with open(file,"r") as f:
 		for each_line in f:
 			if lead_tar in each_line:
 				print(each_line)
-data_search()
+while True:
+	data_search()
+	delay()
