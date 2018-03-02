e61115db7b67dc24d45c6eb70ee35d68ac98127b
dustinduan2010 <dustinduan2010@163.com>
2018-03-02 14:48:07 +0800

    sfis data input basic

--- sfis-data-input.py
+++ sfis-data-input.py
@@ -2,10 +2,10 @@ import time
 import platform as pl
 import os
 def clean_screen():
-	if 'Windows' in pl.platform():
-		os.system("cls")
-	else:
-		os.system("clear screen")
+    if 'Windows' in pl.platform():
+        os.system("cls")
+    else:
+        os.system("clear screen")
 def sfis_time():
     return ("Date:"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n')
 def config_update(file="wo-config.cfg"):
@@ -19,20 +19,19 @@ def config_update(file="wo-config.cfg"):
             with open(file) as f:
                 info=f.readline().strip('\n')
     else:
-        info="Info:"+input("请输入需要会记录的信息:")
+        info="Info:"+input("请输入需要记录的工单信息:")
         with open(file,"w") as f:
             f.write(info)
     clean_screen()
     return (info)
 def sfis_write(inf,file="sfis.txt"):
     sn="MAC:"+input("请输入需要记录的产品条码:").upper()+' '
-    try:
-        with open(file,"a") as f:
-            f.write(info+' '+sn+sfis_time())
+    with open(file,"a") as f:
+        f.write(inf+' '+sn+sfis_time())
         clean_screen()
-        print("sfis记录OK")
-        time.sleep(1.5)
-        clean_screen()
-info_str=config_update()
+    print("sfis记录OK")
+    time.sleep(1.5)
+    clean_screen()
+wo_info=config_update()
 while True:
-    sfis_write(info)
+    sfis_write(wo_info)
